bc40c29e3dedb3c7f9f92e5f66e83e5065f4ff4d
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2025-08-03 22:03:18 +0700

    feat: Add brand logo and favicon to Admin Panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -32,6 +32,8 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Amber,
             ])
+            ->brandLogo(asset('logo.png'))
+            ->favicon(asset('dp.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
