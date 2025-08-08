e785189178d28366e67fb088d917ef8a1fa4f2d6
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2025-08-08 15:07:28 +0700

    feat: Add MSO database connection configuration to database.php

--- config/database.php
+++ config/database.php
@@ -62,6 +62,20 @@ return [
             ]) : [],
         ],
 
+        'mso' => [
+            'driver' => 'mysql',
+            'host' => env('MSO_DB_HOST'),
+            'port' => env('MSO_DB_PORT'),
+            'database' => env('MSO_DB_DATABASE'),
+            'username' => env('MSO_DB_USERNAME'),
+            'password' => env('MSO_DB_PASSWORD'),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => false,
+            'engine' => null,
+        ],
+
         'mariadb' => [
             'driver' => 'mariadb',
             'url' => env('DB_URL'),
@@ -147,7 +161,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')).'-database-'),
+            'prefix' => env('REDIS_PREFIX', Str::slug((string) env('APP_NAME', 'laravel')) . '-database-'),
             'persistent' => env('REDIS_PERSISTENT', false),
         ],
 
