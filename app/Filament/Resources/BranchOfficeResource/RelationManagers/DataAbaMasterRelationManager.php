ca455c7336f817d17d7a8b42577bd96557d7e30c
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2025-08-20 11:20:14 +0700

    feat: Implement read-only mode for DataAbaMasterRelationManager

--- app/Filament/Resources/BranchOfficeResource/RelationManagers/DataAbaMasterRelationManager.php
+++ app/Filament/Resources/BranchOfficeResource/RelationManagers/DataAbaMasterRelationManager.php
@@ -16,6 +16,11 @@ class DataAbaMasterRelationManager extends RelationManager
     protected static string $relationship = 'penempatanABA';
     protected static ?string $title = 'Penempatan Antar Bank';
 
+    public function isReadOnly(): bool
+    {
+        return true;
+    }
+
     public function form(Form $form): Form
     {
         return $form
