3649a0b5c788938ccade0af6d900aa16165dd16d
Haytsam Adzka Mawla <haytsam.adzka@gmail.com>
2025-10-28 18:59:37 +0700

    fix: add encryption settings for MySQL connection in database configuration

--- config/database.php
+++ config/database.php
@@ -100,6 +100,9 @@ return [
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
+
+            'encrypt' => 'yes',
+            'trust_server_certificate' => true,
         ],
 
         'mariadb' => [
